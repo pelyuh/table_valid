f3b2b8d879703d9dc0980c32467991e17bcf3b11
Pelyukh Vitaliy <pelyuh.v@gmail.com>
2019-11-26 22:17:41 -0800

    modified

--- js/table_valid_js.js
+++ js/table_valid_js.js
@@ -2,36 +2,40 @@ function Sum(d) {
 
     var active_cell_id = d.id.split('_');
     var count = active_cell_id.length;
-    var id1;
-    var id2;
-    var id3;
-    var id4;
+
+    var id_1_1_1, id_1_1_2, id_1_1_3, id_1_1_4, id_1_1_5, id_1_1_6, id_1_1_7, id_1_1_8, id_1_1_9;
+    var id_1_1_10, id_1_1_11, id_1_1_12, id_1_1_13, id_1_1_14, id_1_1_15, id_1_1_16, id_1_1_17;
+
+    id_1_1_1 = document.getElementById("1_1_1").value;
+    id_1_1_2 = document.getElementById("1_1_2").value;
+    id_1_1_3 = document.getElementById("1_1_3").value;
+
+    id_1_1_4 = ((id_1_1_1 + id_1_1_2 + id_1_1_3) + 1) / 3;
+    document.getElementById('1_1_4').value = parseFloat(id_1_1_4);
 
 
-    id = active_cell_id[0] + '_' + active_cell_id[1] + '_' + active_cell_id[2];
-    for (var t = 1; t <= 2; t++) {
-        if (active_cell_id[0] == t) {
-            for (var r = 1; r <= 2; r++) {
-                if (active_cell_id[1] == r) {
-                    if (active_cell_id[2] == 1) {
-                        id1 = active_cell_id[0] + '_' + active_cell_id[1] + '_' + active_cell_id[2];
-                    }
-                }
-            }
-        }
-    }
+    id_1_1_5 = document.getElementById("1_1_5").value;
+    id_1_1_6 = document.getElementById("1_1_6").value;
+    id_1_1_7 = document.getElementById("1_1_7").value;
+
+    id_1_1_8 = ((id_1_1_5 + id_1_1_6 + id_1_1_7) + 1) / 3;
+    document.getElementById('1_1_8').value = parseFloat(id_1_1_8);
 
 
+    id_1_1_9 = document.getElementById("1_1_9").value;
+    id_1_1_10 = document.getElementById("1_1_10").value;
+    id_1_1_11 = document.getElementById("1_1_11").value;
 
-        console.log(id1);
-        console.log(id2);
+    id_1_1_12 = ((id_1_1_9 + id_1_1_10 + id_1_1_11) + 1) / 3;
+    document.getElementById('1_1_12').value = parseFloat(id_1_1_12);
 
-    var cell1 = document.getElementById(id1).value;
-    var cell2 = document.getElementById(id2).value;
+    id_1_1_13 = document.getElementById("1_1_13").value;
+    id_1_1_14 = document.getElementById("1_1_14").value;
+    id_1_1_15 = document.getElementById("1_1_15").value;
 
-    id3 = document.getElementById("1_1_3").value;
+    id_1_1_16 = ((id_1_1_13 + id_1_1_14 + id_1_1_15) + 1) / 3;
+    document.getElementById('1_1_16').value = parseFloat(id_1_1_16);
 
-
-    id4 = ((cell1 + cell2 + id3) + 1) / 3;
-    document.getElementById('1_1_4').value = parseFloat(id4);
+    id_1_1_17 = ((id_1_1_4 + id_1_1_8 + id_1_1_12 + id_1_1_16)+1)/4
+    document.getElementById('1_1_17').value = parseFloat(id_1_1_17);
 }
