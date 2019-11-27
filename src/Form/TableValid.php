6673d1fd6fb6b6906bdaae58a25ee7e1be402476
Pelyukh Vitaliy <pelyuh.v@gmail.com>
2019-11-27 13:53:52 -0800

    adds count quarter

--- src/Form/TableValid.php
+++ src/Form/TableValid.php
@@ -68,9 +68,11 @@ class TableValid extends FormBase
         $row_number[$table]++;
 
         $form_state->set('row_number', $row_number);
+
         $form_state->setRebuild();
     }
 
+    // Білдінг форми
     public function buildForm(array $form, FormStateInterface $form_state)
     {
         $table_number = $form_state->get('table_number');
@@ -86,7 +88,6 @@ class TableValid extends FormBase
             $form_state->set('row_number', $row_number);
         }
 
-
         for ($j = 1; $j <= $table_number; $j++) {
 
 
@@ -99,9 +100,6 @@ class TableValid extends FormBase
                 '#submit' => ['::addRow'],
                 '#post' => $table,
                 '#attributes' => ['class' => ['button-add-row']],
-//                '#ajax' => [
-//                    'callback' => '::ajaxUpdateForm',
-//                ],
             ];
 
             // Рендер шапки таблиці
@@ -129,8 +127,6 @@ class TableValid extends FormBase
                 ],
             ];
 
-            $id_row = $row_number[$table] + 1;
-
             //Добавлення рядків
             for ($row = $row_number[$table]; $row > 0; $row--) {
 
@@ -148,41 +144,66 @@ class TableValid extends FormBase
 
                     $id_cell = $j . '_' . $row . '_' . $cell;
 
+                    if (isset($_POST[$id_cell])) {
+
+                        $value = $_POST[$id_cell];
+
+                    } else {
+
+                        $value = '';
+                    }
+
+
                     if ($cell % 4 == 0) {
                         $form[$table][$row][$table_cell_name[$cell]] = [
                             '#type' => 'textfield',
                             '#id' => $id_cell,
-                            '#attributes' => ['class' => ['quarter'],],
-                            '#disabled' => TRUE,
-//                            '#default_value' => '',
-//                            '#ajax' => [
-//                                'callback' => '::ajaxUpdateForm',
-//                            ],
-
-
+                            '#name' => $id_cell,
+                            '#attributes' => [
+                                'class' => ['quarter'],
+                                'readonly' => 'readonly',
+                            ],
+//                            '#disabled' => TRUE,
+                            '#value' => $value,
                         ];
                     } else {
                         $form[$table][$row][$table_cell_name[$cell]] = [
                             '#type' => 'textfield',
                             '#id' => $id_cell,
+                            '#name' => $id_cell,
+                            '#value' => $value,
                             '#attributes' => [
                                 'class' => ['table-head'],
-                                'onchange' => "Sum(this)",
+//                                'onchange' => "Sum(this)",
                             ],
-//                            '#ajax' => [
-//                                'callback' => '::myAjaxCalc',
-//                                'event' => 'change',
-//                            ],
+                            '#ajax' => [
+                                'callback' => '::calc',
+                                'disable-refocus' => TRUE,
+                                'event' => 'change',
+                            ],
                         ];
                     }
                 }
+
+
                 $id_ytd = $j . '_' . $row . '_' . $cell;
+                if (isset($_POST[$id_ytd])) {
+
+                    $value = $_POST[$id_ytd];
+                } else {
+                    $value = '';
+                }
 
                 $form[$table][$row]['YTD'] = [
-                    '#type' => 'number',
+                    '#type' => 'textfield',
                     '#id' => $id_ytd,
-                    '#attributes' => ['class' => ['quarter']],
-                    '#disabled' => TRUE,
+                    '#name' => $id_ytd,
+                    '#value' => $value,
+                    '#attributes' => [
+                        'class' => ['quarter'],
+                        'readonly' => 'readonly',
+                    ],
+//                    '#disabled' => TRUE,
                 ];
             }
 
@@ -191,16 +212,13 @@ class TableValid extends FormBase
         $form['actions']['add_table'] = [
             '#type' => 'submit',
             '#value' => $this->t('Add table'),
-//            '#submit' => ['::addTable'],
-            '#submit' => ['::myAjaxCalc'],
+            '#submit' => ['::addTable'],
         ];
-
 
         $form['actions']['send'] = [
             '#type' => 'submit',
             '#value' => $this->t('Submit'),
         ];
-
 
         return $form;
     }
@@ -215,39 +233,101 @@ class TableValid extends FormBase
 //        }
 //    }
 
+    public function calc(array &$form, FormStateInterface $form_state)
+    {
 
-// действия по сабмиту
+        $id_cell = explode('_', $_POST['_triggering_element_name']);
+
+        $pattern = $id_cell[0] . '_' . $id_cell[1] . '_';
+
+        $id_cell_num = $id_cell[2];
+
+        $ytd = '#' . $id_cell[0] . '_' . $id_cell[1] . '_17';
+
+        if (in_array($id_cell_num, [1, 5, 9, 13])) {
+            $quarter = $_POST[$pattern . $id_cell_num++] + $_POST[$pattern . ($id_cell_num++)] + $_POST[$pattern . ($id_cell_num++)];
+        } elseif (in_array($id_cell_num, [2, 6, 10, 14])) {
+            $quarter = $_POST[$pattern . ($id_cell_num - 1)] + $_POST[$pattern . ($id_cell_num++)] + $_POST[$pattern . ($id_cell_num++)];
+        } else {
+            $quarter = $_POST[$pattern . ($id_cell_num - 1)] + $_POST[$pattern . ($id_cell_num - 2)] + $_POST[$pattern . ($id_cell_num++)];
+        }
+
+        $quarter = round(($quarter + 1) / 3, 2);
+
+
+        $id_quarter = '#' . $pattern . $id_cell_num;
+
+        if ($id_cell_num = 4) {
+            $ytd_sum = $quarter + $_POST[$pattern . '8'] + $_POST[$pattern . '12'] + $_POST[$pattern . '16'];
+        } elseif
+        ($id_cell_num = 8) {
+            $ytd_sum = $_POST[$pattern . '4'] + $quarter + $_POST[$pattern . '12'] + $_POST[$pattern . '16'];
+        } elseif
+        ($id_cell_num = 12) {
+            $ytd_sum = $_POST[$pattern . '4'] + $_POST[$pattern . '8'] + $quarter + $_POST[$pattern . '16'];
+        } elseif
+        ($id_cell_num = 16) {
+            $ytd_sum = $_POST[$pattern . '4'] + $_POST[$pattern . '8'] + $_POST[$pattern . '12'] + $quarter;
+        }
+
+        $ytd_sum = round(($ytd_sum + 1) / 4, 2);
+
+        $sum = new AjaxResponse();
+        $sum->addCommand(new InvokeCommand($id_quarter, 'val', [$quarter]));
+        $sum->addCommand(new InvokeCommand($ytd, 'val', [$ytd_sum]));
+
+        return $sum;
+    }
+
+
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
         // Перевіряємо чи була відправлена форма
         if ($_POST['op'] == 'Submit') {
 
             //Отримуємо кількість таблиць
-            $table_number = $form_state->get('table_number');
+            $table_count = $form_state->get('table_number');
 
-            $cell_valid_number = [1, 2, 3, 5, 6, 7, 9, 10, 11];
+            //Отримуємо кількість рядків в таблицях
+            $row_number = $form_state->get('row_number');
 
-            // Записуємо таблиці із дамини в масив
-            for ($i = 1; $i <= $table_number; $i++) {
-                $table_name = 'table_' . $i;
+            for ($table_num = 1; $table_num <= $table_count; $table_num++) {
+                $row_count = $row_number['table_' . $table_num];
+                for ($row_num = 1; $row_num <= $row_count; $row_num++) {
+                    for ($i = 0; $i <= 4; $i++) {
+                        $quarter1 = $table_num . '_' . $row_num . '_' . 4;
+                        $quarter2 = $table_num . '_' . $row_num . '_' . 8;
+                        $quarter3 = $table_num . '_' . $row_num . '_' . 12;
+                        $quarter4 = $table_num . '_' . $row_num . '_' . 16;
+                        if ($_POST[$quarter1] != ' ' && $_POST[$quarter2] == ' ' && $_POST[$quarter3] == ' ' && $_POST[$quarter4] == ' ') {
 
-                $table_index = substr($table_name, 6);
+                            drupal_set_message($this->t('Valid! Q1'));
 
-                $all_tables[$table_index] = $form_state->getValue($table_name);
+                        } else {
 
-//                for ($id_row = 1; $id_row < )
-                if ('' != $all_tables[$i][1][1] & $all_tables[1][1][2]) {
-                    drupal_set_message($this->t('Valid!'));
-                } else {
-                    drupal_set_message($this->t('Invalid!'));
+//                            drupal_set_message($this->t('Invalid!'));
+                            $form_state->setErrorByName('name', $this->t('Invalid'));
+
+                        }
+                    }
+
                 }
-
-
             }
-            dump($all_tables);
+
+            //Отримуємо кількість таблиць
+
+
+//                if ('' != $all_data[$i][1][1] & $all_data[1][1][2]) {
+//                    drupal_set_message($this->t('Valid!'));
+//                } else {
+//                    drupal_set_message($this->t('Invalid!'));
+//                }
+
+
+        }
 
 //            $form_state->setErrorByName('name', $this->t('Name is too short.'));
-        }
+
 
         $form_state->disableRedirect();
     }
